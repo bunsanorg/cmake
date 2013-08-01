df24efdce8b326d0b42c14936e9f3bdea5598778
Aleksey Filippov <sarum9in@gmail.com>
2013-08-01 18:58:00 +0400

    CMAKE_INSTALL_* support.

--- BunsanCmake/install.cmake
+++ BunsanCmake/install.cmake
@@ -1,16 +1,18 @@
-set(CMAKE_INSTALL_RPATH "$ORIGIN/../lib")
+include(GNUInstallDirs)
+
+set(CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}")
 
 macro(bunsan_install_targets)
     install(TARGETS ${ARGN}
-        RUNTIME DESTINATION bin
-        LIBRARY DESTINATION lib
-        ARCHIVE DESTINATION lib)
+        RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
+        LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
 endmacro()
 
 macro(bunsan_install_programs)
-    install(PROGRAMS ${ARGN} DESTINATION bin)
+    install(PROGRAMS ${ARGN} DESTINATION ${CMAKE_INSTALL_BINDIR})
 endmacro()
 
 macro(bunsan_install_headers)
-    install(DIRECTORY include DESTINATION .)
+    install(DIRECTORY include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 endmacro()
