3545ea597f275776316a8aed5ac5fdfcc4f8723c
Aleksey Filippov <sarum9in@gmail.com>
2014-02-17 19:11:58 +0400

    Tests stack stash.

--- BunsanCmake/tests.cmake
+++ BunsanCmake/tests.cmake
@@ -1,6 +1,34 @@
+macro(bunsan_cmake_tests_prefix_push value)
+    set(bunsan_cmake_tests_prefix_stack
+        ${bunsan_cmake_tests_prefix_stack}
+        ${bunsan_cmake_tests_prefix}
+    )
+    set(bunsan_cmake_tests_prefix ${value})
+endmacro()
+
+function(bunsan_cmake_tests_prefix_pop)
+    list(LENGTH bunsan_cmake_tests_prefix_stack length)
+    if(length EQUAL 0)
+        message(SEND_ERROR "tests prefix stack is empty")
+    endif()
+    math(EXPR pos "${length}-1")
+    list(GET bunsan_cmake_tests_prefix_stack ${pos} bunsan_cmake_tests_prefix)
+    list(REMOVE_AT bunsan_cmake_tests_prefix_stack ${pos})
+endfunction()
+
+macro(bunsan_add_tests)
+    foreach(test_dir ${ARGN})
+        bunsan_cmake_tests_prefix_push(${test_dir}_)
+        add_subdirectory(${test_dir} ${CMAKE_CURRENT_BINARY_DIR}/${test_dir})
+        bunsan_cmake_tests_prefix_pop()
+    endforeach()
+endmacro()
+
 macro(bunsan_include_tests)
     if(ENABLE_TESTS)
         enable_testing()
+        set(bunsan_cmake_tests_prefix_stack)
+        set(bunsan_cmake_tests_prefix test_)
         add_subdirectory(tests ${CMAKE_CURRENT_BINARY_DIR}/tests)
     endif()
 endmacro()
@@ -28,12 +56,12 @@ endmacro()
 
 # updates ${bunsan_tests_targets} and empty ${bunsan_tests_sources}
 macro(bunsan_tests_project_add_test source target)
-    add_executable(test_${target} ${source})
-    target_link_libraries(test_${target} ${CMAKE_PROJECT_NAME} ${libraries})
-    add_test(${target} test_${target})
-    set_tests_properties(${target} PROPERTIES ENVIRONMENT "${test_env}")
+    add_executable(${bunsan_cmake_tests_prefix}${target} ${source})
+    target_link_libraries(${bunsan_cmake_tests_prefix}${target} ${CMAKE_PROJECT_NAME} ${libraries})
+    add_test(${bunsan_cmake_tests_prefix}${target} ${bunsan_cmake_tests_prefix}${target})
+    set_tests_properties(${bunsan_cmake_tests_prefix}${target} PROPERTIES ENVIRONMENT "${test_env}")
     list(APPEND bunsan_tests_sources ${source})
-    list(APPEND bunsan_tests_targets ${target})
+    list(APPEND bunsan_tests_targets ${bunsan_cmake_tests_prefix}${target})
 endmacro()
 
 # updates ${bunsan_tests_targets} and empty ${bunsan_tests_sources}
