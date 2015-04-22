d601381f7f0459468e270ba81b2b7da06dd8735b
Aleksey Filippov <sarum9in@gmail.com>
2015-04-22 23:38:52 +0400

    Fixed bunsan_install_python_*().

--- scripts/install_python_module.cmake
+++ scripts/install_python_module.cmake
@@ -1,37 +1,57 @@
+function(bunsan_get_install_python_path path)
+    set(python_version ${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR})
+
+    if(UNIX)
+        set(${path} lib/python${python_version}/site-packages PARENT_SCOPE)
+    else()
+        message(SEND_ERROR "Environment is not supported.")
+    endif()
+endfunction()
+
+function(bunsan_get_install_python_package_components)
+    set(options)
+    set(one_value_args PACKAGE DIRECTORY NAME PATH)
+    set(multi_value_args)
+    cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+    bunsan_required_arguments(ARG PACKAGE)
+
+    string(REPLACE "." "/" package ${ARG_PACKAGE})
+    get_filename_component(package_name ${package} NAME)
+    get_filename_component(package_directory ${package} DIRECTORY)
+
+    bunsan_get_install_python_path(python_path)
+
+    if(ARG_DIRECTORY)
+        set(${ARG_DIRECTORY} ${python_path}/${package_directory} PARENT_SCOPE)
+    endif()
+    if(ARG_NAME)
+        set(${ARG_NAME} ${package_name} PARENT_SCOPE)
+    endif()
+    if(ARG_PATH)
+        set(${ARG_PATH} ${python_path}/${package} PARENT_SCOPE)
+    endif()
+endfunction()
+
 macro(bunsan_install_python_module_common source_arg)
     set(options)
     set(one_value_args ${source_arg} MODULE)
     set(multi_value_args)
     cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-    if(ARG_MODULE)
-        set(module ${ARG_MODULE})
-    else()
-        message(SEND_ERROR "MODULE is not set.")
-    endif()
-    if(NOT ARG_${source_arg})
-        message(SEND_ERROR "${source_arg} is not set.")
-    endif()
+    bunsan_required_arguments(ARG ${one_value_args})
 
-    string(REPLACE "." "/" module ${module})
-    get_filename_component(module_name ${module} NAME)
-    get_filename_component(module_path ${module} PATH)
+    bunsan_get_install_python_package_components(
+        PACKAGE ${ARG_MODULE}
+        DIRECTORY module_directory
+        NAME module_name
+        PATH module_path
+    )
 
-    set(python_version ${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR})
-
-    if(UNIX)
-        set(destination lib/python${python_version}/site-packages)
-    else()
-        message(SEND_ERROR "Environment is not supported.")
-    endif()
-
-    set(module_destination ${destination}/${module_path})
-
-    string(REPLACE "/" ";" module_destination_list ${module_destination})
-    list(LENGTH module_destination_list length)
+    string(REPLACE "/" ";" module_directory_split ${module_directory})
+    list(LENGTH module_directory_split length)
     math(EXPR last "${length} - 1")
-    list(REMOVE_AT module_destination_list last)
+    list(REMOVE_AT module_directory_split last)
     set(base_rpath "$ORIGIN")
-    foreach(part ${module_destination_list})
+    foreach(part ${module_directory_split})
         set(base_rpath "${base_rpath}/..")
     endforeach()
 
@@ -48,12 +68,13 @@ function(bunsan_install_python_module_target)
         INSTALL_RPATH "${rpath}"
     )
 
-    install(TARGETS ${ARG_TARGET} DESTINATION ${module_destination})
+    install(TARGETS ${ARG_TARGET} DESTINATION ${module_directory})
 endfunction()
 
 function(bunsan_install_python_module_compile_one pyfile)
     string(REPLACE ";" " " pyflags "${ARGN}")
     install(CODE "
+        message(\"-- Compiling python source [${ARGN}]: ${pyfile}\")
         set(pyfile \"\$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/${pyfile}\")
         execute_process(
             COMMAND ${PYTHON_EXECUTABLE} ${pyflags} -m py_compile \${pyfile}
@@ -77,13 +98,34 @@ endfunction()
 function(bunsan_install_python_module_init)
     bunsan_install_python_module_common(INIT ${ARGN})
 
-    install(FILES ${ARG_INIT} DESTINATION ${destination}/${module} RENAME __init__.py)
-    bunsan_install_python_module_compile(${destination}/${module}/__init__.py)
+    install(FILES ${ARG_INIT} DESTINATION ${module_directory} RENAME __init__.py)
+    bunsan_install_python_module_compile(${module_directory}/__init__.py)
 endfunction()
 
 function(bunsan_install_python_module_file)
     bunsan_install_python_module_common(FILE ${ARGN})
 
-    install(FILES ${ARG_FILE} DESTINATION ${destination}/${module_path} RENAME ${module_name}.py)
-    bunsan_install_python_module_compile(${destination}/${module}.py)
+    install(FILES ${ARG_FILE} DESTINATION ${module_directory} RENAME ${module_name}.py)
+    bunsan_install_python_module_compile(${module_path}.py)
+endfunction()
+
+function(bunsan_install_python_package)
+    set(options)
+    set(one_value_args PACKAGE DIRECTORY)
+    set(multi_value_args)
+    cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+    bunsan_required_arguments(ARG ${one_value_args})
+
+    bunsan_get_install_python_package_components(
+        PACKAGE ${ARG_PACKAGE}
+        PATH path
+    )
+    install(DIRECTORY ${ARG_DIRECTORY}/ DESTINATION ${path})
+    file(GLOB_RECURSE pyfiles
+        RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_DIRECTORY}
+        ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_DIRECTORY}/*.py
+    )
+    foreach(pyfile ${pyfiles})
+        bunsan_install_python_module_compile(${path}/${pyfile})
+    endforeach()
 endfunction()
