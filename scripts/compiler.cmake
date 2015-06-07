3abbd0b598a7d1dea07dc0f976da0ee674ab839d
Aleksey Filippov <sarum9in@gmail.com>
2015-06-07 06:33:16 +0400

    Removed -Wno-multichar compiler flag.

--- scripts/compiler.cmake
+++ scripts/compiler.cmake
@@ -1,5 +1,5 @@
 if(CMAKE_COMPILER_IS_GNUCXX)
-    set(CMAKE_CXX_FLAGS "-std=c++14 -Wall -Wpedantic -Wextra -Wno-multichar")
+    set(CMAKE_CXX_FLAGS "-std=c++14 -Wall -Wpedantic -Wextra")
     if(UNIX)
         set(CMAKE_CXX_FLAGS "-rdynamic ${CMAKE_CXX_FLAGS}")
     endif()
