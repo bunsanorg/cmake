ff3c7051ca29865f7c222c68a22a8164588eb9ba
Aleksey Filippov <sarum9in@gmail.com>
2015-09-01 14:42:27 +0400

    Fixed FindGrpc: added gpr library.

--- modules/FindGrpc.cmake
+++ modules/FindGrpc.cmake
@@ -1,4 +1,5 @@
 find_path(GRPC_INCLUDE_DIR grpc/grpc.h)
+find_library(GPR_LIBRARY gpr)
 find_library(GRPC_LIBRARY grpc)
 find_library(GRPCXX_LIBRARY grpc++)
 
@@ -7,12 +8,13 @@ find_program(GRPC_CXX_PLUGIN grpc_cpp_plugin)
 find_program(GRPC_PYTHON_PLUGIN grpc_python_plugin)
 
 if(GRPC_INCLUDE_DIR AND
+   GPR_LIBRARY AND
    GRPC_LIBRARY AND
    GRPCXX_LIBRARY AND
    GRPC_CXX_PLUGIN AND
    GRPC_PYTHON_PLUGIN)
     set(Grpc_INCLUDE_DIRS ${GRPC_INCLUDE_DIR})
-    set(Grpc_LIBRARIES ${GRPC_LIBRARY} ${GRPCXX_LIBRARY})
+    set(Grpc_LIBRARIES ${GPR_LIBRARY} ${GRPC_LIBRARY} ${GRPCXX_LIBRARY})
     set(Grpc_CXX_PLUGIN ${GRPC_CXX_PLUGIN})
     set(Grpc_PYTHON_PLUGIN ${GRPC_PYTHON_PLUGIN})
 endif()
