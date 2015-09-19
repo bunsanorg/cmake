a959213506f58f914e43f84b34b244ade87916d0
Aleksey Filippov <sarum9in@gmail.com>
2015-09-19 20:13:04 +0400

    Refactored bunsan_add_protobuf_cxx_library().

--- scripts/protobuf_generate.cmake
+++ scripts/protobuf_generate.cmake
@@ -20,7 +20,7 @@ endmacro()
 
 macro(bunsan_protobuf_append_proto_paths var)
     foreach(proto_path ${ARGN})
-        list(APPEND ${var} --proto_path=${proto_path})
+        list(APPEND ${var} ${proto_path})
     endforeach()
     bunsan_list_remove_duplicates(${var})
 endmacro()
@@ -53,6 +53,136 @@ function(bunsan_protobuf_get_libraries_proto_paths proto_paths)
     set(${proto_paths} ${proto_paths_} PARENT_SCOPE)
 endfunction()
 
+# OUTPUT is list
+function(bunsan_protobuf_get_names)
+    set(options)
+    set(one_value_args PLUGIN PROTO
+        OUTPUT PREFIX NAME
+    )
+    set(multi_value_args)
+    cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+    get_filename_component(filename_we ${ARG_PROTO} NAME_WE)
+    get_filename_component(dirname ${ARG_PROTO} DIRECTORY)
+    set(path_we ${dirname}/${filename_we})
+    if(ARG_PLUGIN STREQUAL cpp)
+        # make header first, used in bunsan_protobuf_install
+        set(output ${path_we}.pb.h ${path_we}.pb.cc)
+    elseif(ARG_PLUGIN STREQUAL grpc_cpp)
+        # make header first, used in bunsan_protobuf_install
+        set(output ${path_we}.grpc.pb.h ${path_we}.grpc.pb.cc)
+    elseif(ARG_PLUGIN STREQUAL python)
+        set(output ${path_we}_pb2.py)
+    elseif(ARG_PLUGIN STREQUAL grpc_python)
+        set(output) # merged with python
+    elseif(ARG_PLUGIN STREQUAL go)
+        set(output ${path_we}.go)
+    else()
+        message(SEND_ERROR "Unknown protoc plugin ${ARG_PLUGIN}")
+    endif()
+    if(ARG_OUTPUT)
+        set(${ARG_OUTPUT} ${output} PARENT_SCOPE)
+    endif()
+    if(ARG_PREFIX)
+        set(${ARG_PREFIX} ${dirname} PARENT_SCOPE)
+    endif()
+    if(ARG_NAME)
+        set(${ARG_NAME} ${filename_we} PARENT_SCOPE)
+    endif()
+endfunction()
+
+function(bunsan_protobuf_install_headers PLUGIN BINARY_PREFIX PROTO)
+    bunsan_protobuf_get_names(
+        PLUGIN ${PLUGIN}
+        PROTO ${PROTO}
+        OUTPUT output
+        PREFIX prefix
+        NAME name
+    )
+    if(PLUGIN STREQUAL cpp OR
+       PLUGIN STREQUAL grpc_cpp)
+        # header
+        list(GET output 0 header)
+        install(FILES ${BINARY_PREFIX}/${header}
+                DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${prefix})
+    elseif(PLUGIN STREQUAL python)
+        # assumes that len(output) == 1
+        bunsan_install_python_module_file(MODULE ${prefix}/${name}_pb2
+                                          FILE ${BINARY_PREFIX}/${output})
+    elseif(PLUGIN STREQUAL go)
+        # does nothing
+    else()
+        message(SEND_ERROR "Unknown protoc plugin ${PLUGIN}")
+    endif()
+endfunction()
+
+function(bunsan_add_protobuf_generate)
+    bunsan_find_package(Protobuf REQUIRED)
+
+    set(options INSTALL_HEADERS)
+    set(one_value_args
+        TARGET
+        SOURCE_PREFIX BINARY_PREFIX
+    )
+    set(multi_value_args PLUGINS PROTOS PROTO_PATHS PLUGIN_PARAMS OUTPUT)
+    cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+    file(MAKE_DIRECTORY ${ARG_BINARY_PREFIX})
+
+    set(protoc_params)
+
+    foreach(proto_path ${ARG_PROTO_PATHS})
+        list(APPEND protoc_params --proto_path=${proto_path})
+    endforeach()
+
+    set(plugin_names)
+    foreach(plugin ${ARG_PLUGINS})
+        string(FIND ${plugin} "=" eqpos)
+        set(plugin_params "")
+        foreach(plugin_param ${ARG_PLUGIN_PARAMS})
+            set(plugin_params "${plugin_params}${plugin_param}:")
+        endforeach()
+        string(SUBSTRING ${plugin} 0 ${eqpos} plugin_name)
+        list(APPEND protoc_params
+            "--plugin=protoc-gen-${plugin}"
+            "--${plugin_name}_out=${plugin_params}${ARG_BINARY_PREFIX}"
+        )
+        list(APPEND plugin_names ${plugin_name})
+    endforeach()
+
+    set(sources)
+    set(output)
+    foreach(proto ${ARG_PROTOS})
+        list(APPEND sources ${ARG_SOURCE_PREFIX}/${proto})
+        foreach(plugin_name ${plugin_names})
+            bunsan_protobuf_get_names(
+                PLUGIN ${plugin_name}
+                PROTO ${proto}
+                OUTPUT proto_outs
+            )
+            foreach(proto_out ${proto_outs})
+                list(APPEND output ${ARG_BINARY_PREFIX}/${proto_out})
+            endforeach()
+            if(ARG_INSTALL_HEADERS)
+                    bunsan_protobuf_install_headers(
+                        ${plugin_name} ${ARG_BINARY_PREFIX} ${proto})
+            endif()
+        endforeach()
+    endforeach()
+
+    add_custom_command(
+        OUTPUT ${output}
+        COMMAND ${PROTOBUF_PROTOC_EXECUTABLE} ${protoc_params} ${sources}
+        DEPENDS ${sources}
+    )
+    if(ARG_TARGET)
+        add_custom_target(${ARG_TARGET} ALL DEPENDS ${output})
+    endif()
+    if(ARG_OUTPUT)
+        set(${ARG_OUTPUT} ${output} PARENT_SCOPE)
+    endif()
+endfunction()
+
 # bunsan_add_protobuf_cxx_library(
 #     INSTALL install headers and library
 #     PYTHON also build python library
@@ -66,30 +196,17 @@ endfunction()
 #     EXPORT_MACRO_NAME LIBRARY_EXPORT (dllexport code)
 #     PLUGINS plugin1=exe plugin2=exe2
 #     PROTOS *.proto
-#     HEADERS var (list of generated headers)
-#     SOURCES var (list of generated sources)
 # )
 function(bunsan_add_protobuf_cxx_library)
     bunsan_find_package(Protobuf REQUIRED)
 
-    string(SHA1 call_hash "${ARGN}")
     set(options INSTALL PYTHON STATIC SHARED GRPC)
-    set(one_value_args
-        TARGET
-        HEADERS SOURCES PYTHON_SOURCES DESCRIPTOR_SET DESCRIPTOR_SET_FILENAME
-        EXPORT_MACRO_NAME
-    )
-    set(multi_value_args PROTOS PLUGINS
+    set(one_value_args TARGET EXPORT_MACRO_NAME)
+    set(multi_value_args
+        PROTOS PLUGINS
         INCLUDE_DIRECTORIES LIBRARIES
     )
     cmake_parse_arguments(ARG "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-
-    set(proto_src ${CMAKE_CURRENT_SOURCE_DIR}/include)
-    set(proto_dst ${CMAKE_CURRENT_BINARY_DIR}/bunsan_protobuf)
-    set(hdrs_)
-    set(srcs_)
-    set(python_srcs_)
-    set(protos_)
 
     if(ARG_STATIC)
         set(libtype STATIC)
@@ -97,133 +214,77 @@ function(bunsan_add_protobuf_cxx_library)
         set(libtype SHARED)
     endif()
 
-    # cpp parameters
-    set(cpp_params)
-    if(ARG_EXPORT_MACRO_NAME)
-        set(cpp_params "${cpp_params}dllexport_decl=${ARG_EXPORT_MACRO_NAME}:")
+    set(generate_params)
+    if(ARG_INSTALL)
+        list(APPEND generate_params INSTALL_HEADERS)
     endif()
 
-    # plugins
-    set(plugins)
-    foreach(plugin ${ARG_PLUGINS})
-        string(FIND ${plugin} "=" eqpos)
-        string(SUBSTRING ${plugin} 0 ${eqpos} plugin_name)
-        list(APPEND plugins
-            "--plugin=protoc-gen-${plugin}"
-            "--${plugin_name}_out=${cpp_params}${proto_dst}"
-        )
-    endforeach()
-
-    # gRPC
-    if(ARG_GRPC)
-        find_package(Grpc REQUIRED)
-        list(APPEND plugins
-            "--plugin=protoc-gen-grpc=${Grpc_CXX_PLUGIN}"
-            "--grpc_out=${cpp_params}${proto_dst}"
-        )
-    endif()
-
-    # compose descriptor set
-    set(descriptor_set_dir ${proto_dst}/${call_hash})
-    if(ARG_DESCRIPTOR_SET_FILENAME)
-        set(descriptor_set_ ${ARG_DESCRIPTOR_SET_FILENAME})
-    else()
-        set(descriptor_set_ "descriptor_set_${call_hash}.pb")
-    endif()
-    set(descriptor_set_ ${descriptor_set_dir}/${descriptor_set_})
-
-    # compose sources and results
-    foreach(proto ${ARG_PROTOS})
-        get_filename_component(filename_we ${proto} NAME_WE)
-        get_filename_component(dirname ${proto} PATH)
-        set(path_we ${dirname}/${filename_we})
-        set(hdr ${proto_dst}/${path_we}.pb.h)
-        set(src ${proto_dst}/${path_we}.pb.cc)
-        set(grpc_hdr ${proto_dst}/${path_we}.grpc.pb.h)
-        set(grpc_src ${proto_dst}/${path_we}.grpc.pb.cc)
-        set(prt ${proto_src}/${proto})
-        set(pysrc ${proto_dst}/${path_we}_pb2.py)
-        list(APPEND hdrs_ ${hdr})
-        list(APPEND srcs_ ${src})
-        if(ARG_PYTHON)
-            list(APPEND python_srcs_ ${pysrc})
-        endif()
-        if(ARG_GRPC)
-            list(APPEND hdrs_ ${grpc_hdr})
-            list(APPEND srcs_ ${grpc_src})
-        endif()
-        list(APPEND protos_ ${prt})
-        if(ARG_INSTALL)
-            install(FILES ${hdr}
-                    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${dirname})
-            if(ARG_GRPC)
-                install(FILES ${grpc_hdr}
-                        DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/${dirname})
-            endif()
-            if(ARG_PYTHON)
-                bunsan_install_python_module_file(MODULE ${path_we}_pb2 FILE ${pysrc})
-            endif()
-        endif()
-    endforeach()
-
-    # compose includes
     set(proto_paths)
     bunsan_protobuf_append_proto_paths(proto_paths
         ${ARG_INCLUDE_DIRECTORIES}
         ${CMAKE_CURRENT_SOURCE_DIR}/include # bunsan project layout
-        ${proto_dst}
     )
     bunsan_protobuf_get_libraries_proto_paths(proto_paths_ ${ARG_LIBRARIES})
     list(APPEND proto_paths ${proto_paths_})
 
-    # python
-    if(ARG_PYTHON)
-        set(python_args --python_out=${proto_dst})
+    set(cpp_params)
+    if(ARG_EXPORT_MACRO_NAME)
+        list(APPEND cpp_params "dllexport_decl=${ARG_EXPORT_MACRO_NAME}")
     endif()
 
-    # generate
-    bunsan_create_directories(${proto_dst} ${descriptor_set_dir})
-    add_custom_command(
-        OUTPUT ${hdrs_} ${srcs_} ${python_srcs_} ${descriptor_set_}
-        COMMAND ${PROTOBUF_PROTOC_EXECUTABLE}
-            --cpp_out=${cpp_params}${proto_dst} ${plugins}
-            ${python_args}
-            --descriptor_set_out=${descriptor_set_}
-            ${proto_paths} ${protos_}
-        DEPENDS ${protos_}
+    # C++
+    set(cxx_plugins cpp)
+    if(ARG_GRPC)
+        find_package(Grpc REQUIRED)
+        list(APPEND cxx_plugins grpc_cpp=${Grpc_CXX_PLUGIN})
+    endif()
+    bunsan_add_protobuf_generate(
+        # TARGET
+        PLUGINS ${cxx_plugins}
+        SOURCE_PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/include
+        BINARY_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/bunsan_protobuf/cxx
+        PROTOS ${ARG_PROTOS}
+        PROTO_PATHS ${proto_paths}
+        PLUGIN_PARAMS ${cpp_params}
+        OUTPUT cpp_output
+        ${generate_params}
     )
-
-    # main target
-    bunsan_add_library(${ARG_TARGET} ${libtype} ${hdrs_} ${srcs_})
-    bunsan_protobuf_include_directories(${ARG_TARGET}
-        PUBLIC
-            ${PROTOBUF_INCLUDE_DIRS}
-            ${ARG_INCLUDE_DIRECTORIES}
-            $<BUILD_INTERFACE:${proto_dst}>
+    bunsan_add_library(${ARG_TARGET} ${libtype} ${cpp_output})
+    bunsan_protobuf_include_directories(${ARG_TARGET} PUBLIC
+        ${PROTOBUF_INCLUDE_DIRS}
+        ${ARG_INCLUDE_DIRECTORIES}
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/bunsan_protobuf/cxx>
     )
-    bunsan_protobuf_link_libraries(${ARG_TARGET}
-        PUBLIC
-            ${PROTOBUF_LIBRARIES}
-            ${ARG_LIBRARIES}
+    bunsan_protobuf_link_libraries(${ARG_TARGET} PUBLIC
+        ${PROTOBUF_LIBRARIES}
+        ${ARG_LIBRARIES}
     )
     if(ARG_GRPC)
         target_include_directories(${ARG_TARGET} PUBLIC ${Grpc_INCLUDE_DIRS})
         target_link_libraries(${ARG_TARGET} PUBLIC ${Grpc_LIBRARIES})
     endif()
 
+    if(ARG_PYTHON)
+        set(python_plugins python)
+        if(ARG_GRPC)
+            list(APPEND python_plugins grpc_python=${Grpc_PYTHON_PLUGIN})
+        endif()
+        bunsan_add_protobuf_generate(
+            TARGET ${ARG_TARGET}_python
+            PLUGINS ${python_plugins}
+            SOURCE_PREFIX ${CMAKE_CURRENT_SOURCE_DIR}/include
+            BINARY_PREFIX ${CMAKE_CURRENT_BINARY_DIR}/bunsan_protobuf/python
+            PROTOS ${ARG_PROTOS}
+            PROTO_PATHS ${proto_paths}
+            # PLUGIN_PARAMS
+            # OUTPUT
+            ${generate_params}
+        )
+        # Make sure that make TARGET also builds python
+        add_dependencies(${ARG_TARGET} ${ARG_TARGET}_python)
+    endif()
+
     # exports
-    if(ARG_HEADERS)
-        set(${ARG_HEADERS} ${hdrs_} PARENT_SCOPE)
-    endif()
-    if(ARG_SOURCES)
-        set(${ARG_SOURCES} ${srcs_} PARENT_SCOPE)
-    endif()
-    if(ARG_PYTHON_SOURCES)
-        set(${ARG_PYTHON_SOURCES} ${python_srcs_} PARENT_SCOPE)
-    endif()
-    if(ARG_DESCRIPTOR_SET)
-        set(${ARG_DESCRIPTOR_SET} ${descriptor_set_} PARENT_SCOPE)
-    endif()
     if(ARG_INSTALL)
         bunsan_install_targets(${ARG_TARGET})
     else()
